865f3a6543655ca9c7b18b917a18a3d542e19f02
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-18 09:05:42 -0700

    get restaurant object from backend api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import api from "./api/axiosConfig"
+import { useState, useEffect } from "react";
+
 function App() {
+
+  const [restaurants, setRestaurants] = useState();
+
+  const getRestaurants = async () => {
+    try {
+      const response = await api.get("/api/v1/restaurants")
+      setRestaurants(response.data)
+      console.log(restaurants);
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getRestaurants();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        Hello
+        Restaurant Review
       </header>
     </div>
   );
