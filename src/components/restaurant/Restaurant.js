12f9ec21dfce1d0d139460bb4dc0690a7146dd40
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-19 21:09:36 -0700

    buttons and stars

--- src/components/restaurant/Restaurant.js
+++ src/components/restaurant/Restaurant.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./Restaurant.css";
+import Star from "../media/Star.png"
 
 const Restaurant = ({ restaurants }) => {
+
+  function handleClick(e) {
+    console.log("clicked");
+  }
+
   return (
     <div className="restaurant-container">
       {restaurants?.map((restaurant) => {
@@ -13,9 +19,44 @@ const Restaurant = ({ restaurants }) => {
               </div>
               <div className="restaurant-information">
                 <div className="restaurant-name">{restaurant.name}</div>
-                <div className="restaurant-type">{restaurant.type}</div>
-                <div className="restaurant-address">{restaurant.address}</div>
-                <div className="restaurant-cost">{restaurant.cost}</div>
+                <div className="type-cost"> 
+                  <div className="restaurant-type">{restaurant.type}</div>
+                  {"·"}
+                  <div className="restaurant-cost-container"> 
+                    <div className="restaurant-cost">{restaurant.cost}</div>
+                  </div>
+                  {"·"}
+                  <div className="star-container">
+                    <img className="star" src={Star}></img>
+                    <img className="star" src={Star}></img>
+                    <img className="star" src={Star}></img>
+                    <img className="star" src={Star}></img>
+                    <img className="star" src={Star}></img>
+                  </div>
+                </div>
+                <div className="address-phone"> 
+                  <div className="restaurant-address">{restaurant.address}</div>
+                  {"·"}
+                  <div className="restaurant-phone">{restaurant.phone}</div>
+                </div>
+                <div className="restaurant-description">{restaurant.description}</div>
+                <div className="button-container"> 
+                  <div className="write-container"> 
+                    <button className="write" onClick={handleClick}>
+                      Write A Review
+                    </button>
+                  </div>
+                  <div className="review-container"> 
+                    <button className="review" onClick={handleClick}>
+                      Read Reviews
+                    </button>
+                  </div>
+                  <div className="website-container"> 
+                    <button className="website" onClick={handleClick}>
+                      Website
+                    </button>  
+                  </div>
+                </div>
               </div>
             </div>
           </div>
